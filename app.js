efdb56ae9391ee69e6dafb1e2e059a88b76b11f3
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-09-15 15:49:09 -0700

    architecture images displaying

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 /* Imports */
 
 /* Get DOM Elements */
-// const displayArticle = document.getElementById('display-article');
+const displayArticle = document.getElementById('display-article');
 const cityName = document.getElementById('city-name');
-// const climateSelect = document.getElementById('climate-select');
-// const colorSelect = document.getElementById('color-select');
-// const architectureSelect = document.getElementById('architecture-select');
+const climateSelect = document.getElementById('climate-select');
+const colorSelect = document.getElementById('color-select');
+const architectureSelect = document.getElementById('architecture-select');
 const nameDisplay = document.getElementById('name-display');
-// const climateDisplay = document.getElementById('climate-display');
-// const architectureDisplay = document.getElementById('architecture-display');
+const climateDisplay = document.getElementById('climate-display');
+const architectureDisplay = document.getElementById('architecture-display');
 // const attractionsDisplay = document.getElementById('attractions-display');
 // addAttractionButton = document.getElementById('add-attraction-button');
 
 /* State */
 let city = {
     name: '',
-    //     climate: '',
-    //     color: '',
-    //     architecture: '',
+    climate: '',
+    color: '',
+    architecture: '',
     //     attractions: [],
 };
 
@@ -28,9 +28,27 @@ cityName.addEventListener('input', () => {
     displayCity();
 });
 
+climateSelect.addEventListener('change', () => {
+    city.climate = climateSelect.value;
+    displayCity();
+});
+
+colorSelect.addEventListener('change', () => {
+    city.color = colorSelect.value;
+    displayCity();
+});
+
+architectureSelect.addEventListener('change', () => {
+    city.architecture = architectureSelect.value;
+    displayCity();
+});
+
 /* Display Functions */
 function displayCity() {
     nameDisplay.textContent = city.name;
+    climateDisplay.src = 'images/climate/' + city.climate + '.png';
+    displayArticle.classList = city.color;
+    architectureDisplay.src = 'images/architecture/' + city.architecture + '.png';
 }
 
 // (don't forget to call any display functions you want to run on page load!)
